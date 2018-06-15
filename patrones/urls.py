a1719bbebb13b8793a13d4a531b095ea29053616
Rygo <rygo.bustos@gmail.com>
2018-06-14 22:34:19 -0400

    Finalizado con los 3 patrones

--- patrones/urls.py
+++ patrones/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from patrones import views
 
 urlpatterns = [
-    path('menu', views.menu, name="menu"),
+    path('', views.menu, name="menu"),
     path('mf', views.mf, name="mf"),
+    path('cd', views.cd, name="cd"),
+    path('ls', views.ls, name="ls"),
 ]
--- patrones/views.py
+++ patrones/views.py
@@ -11,3 +11,13 @@ def mf(request):
     data = {}
     template_name='mostly_fluid.html'
     return render(request, template_name,data)
+
+def cd(request):
+    data = {}
+    template_name='column_drop.html'
+    return render(request, template_name,data)
+
+def ls(request):
+    data = {}
+    template_name='layout_shifter.html'
+    return render(request, template_name,data)
